a6266bedc6ea180728992863f8eb7615c0d0b764
bramadityaw <rbramadityaario@gmail.com>
2024-11-05 12:47:49 +0700

    feat: align User model to user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'password',
     ];
 
@@ -30,7 +29,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -39,7 +37,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 }
