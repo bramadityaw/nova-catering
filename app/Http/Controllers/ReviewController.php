7756f77a1729bd74c2c638434e78495c006ec67e
bramadityaw <rbramadityaario@gmail.com>
2024-11-06 12:19:54 +0700

    feat: create and retrieve reviews

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ReviewController extends Controller
+{
+    public function all() : JsonResponse
+    {
+        $reviews = DB::table('reviews')
+            ->where('public', true)
+            ->select('id', 'reviewer_name', 'content')
+            ->get();
+        return response()->json($reviews);
+    }
+
+    public function store(Request $request) : JsonResponse
+    {
+        $validated = $request->validate([
+            'content' => ['required'],
+            'reviewer_name' => ['required'],
+        ]);
+
+        $review = new Review();
+        $review->content = $validated['content'];
+        $review->reviewer_name = $validated['reviewer_name'];
+
+        if (!$review->save()) {
+            return response()->json([
+                'message' => 'Internal server error'
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => "Ulasan milik {$validated['reviewer_name']} berhasil disimpan"
+        ]);
+    }
+}
