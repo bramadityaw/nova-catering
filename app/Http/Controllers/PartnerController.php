d7e00177012f41775393e4d45f1922b6c8419341
bramadityaw <rbramadityaario@gmail.com>
2024-11-06 17:32:56 +0700

    feat: create, read, and delete partners

---
+++ app/Http/Controllers/PartnerController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Partner;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PartnerController extends Controller
+{
+    public function all() : JsonResponse
+    {
+        $partners = DB::table('partners')
+            ->select('nama', 'logo')
+            ->get();
+
+        return response()->json($partners);
+    }
+
+    public function index() : JsonResponse
+    {
+        return response()->json(Partner::all());
+    }
+
+    public function show(Partner $partner) : JsonResponse
+    {
+        return response()->json($partner);
+    }
+
+    public function store(Request $request) : JsonResponse
+    {
+        $validated = $request->validate([
+            'logo' => [
+                'required',
+                'mimes:jpg,jpeg,png',
+                'extensions:jpg,jpeg,png'
+            ],
+            'nama' => ['required'],
+        ]);
+
+        $nama = $validated['nama'];
+
+        $file = $request->file('logo');
+
+        if (! $file->isValid()) {
+            return response()->json([
+                'message' => 'File gagal terupload.'
+            ], 400);
+        }
+
+        $ext = $file->extension();
+        $path = $file->storeAs('partners', "$nama.$ext", 'public');
+
+        $partner = new Partner();
+        $partner->nama = $nama;
+        $partner->logo = asset('/storage/' . $path);
+
+        if (! $partner->save()) {
+            return response()->json([
+                'message' => "Partner $nama gagal tersimpan.",
+                'logo_path' => $path,
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => "Partner $nama tersimpan.",
+        ]);
+    }
+
+    public function update(Request $request,Partner $partner) : JsonResponse
+    {
+        return response()->json();
+    }
+
+    public function destroy(Partner $partner) : JsonResponse
+    {
+        $nama = $partner->nama;
+
+        if (! $partner->delete()) {
+            return response()->json([
+                'message' => "Partner $nama gagal terhapus.",
+            ]);
+        }
+
+        return response()->json([
+            'message' => "Partner $nama berhasil terhapus.",
+        ]);
+    }
+}
