a6c1eac6ee7f8ad33393d8e6a77cf14002f4629b
Vunix99 <rian.beda246@gmail.com>
2024-11-14 16:44:45 +0700

    feat: add new api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\PaketController;
 use App\Http\Controllers\PartnerController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\SatuanController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DataCountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,8 +27,12 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+    Route::get('/user/name', [UserController::class, 'getNameById']);
+    Route::get('/user', [UserController::class, 'getAllNames']);
+
+    Route::get('/total/data', [DataCountController::class, 'countData']);
+            
     Route::get('/reviews/index', [ReviewController::class, 'index'])->name('review.index');
-    Route::post('/review', [ReviewController::class, 'store'])->name('review.store');
     Route::post('/review/{review}/hide', [ReviewController::class, 'conceal'])->name('review.conceal');
     Route::post('/review/{review}/show', [ReviewController::class, 'reveal'])->name('review.reveal');
     Route::delete('/review/{review}', [ReviewController::class, 'destroy'])->name('review.destroy');
@@ -37,19 +43,23 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/partner/{partner}', [PartnerController::class, 'update'])->name('partner.update');
 
     Route::get('/satuans/index', [SatuanController::class, 'index'])->name('satuan.index');
+    Route::get('/satuan/{id}', [SatuanController::class, 'getById'])->name('satuan.getById');
     Route::post('/satuan', [SatuanController::class, 'store'])->name('satuan.store');
     Route::delete('/satuan/{satuan}', [SatuanController::class, 'destroy'])->name('satuan.destroy');
     Route::put('/satuan/{satuan}', [SatuanController::class, 'update'])->name('satuan.update');
 
     Route::get('/pakets/index', [PaketController::class, 'index'])->name('paket.index');
     Route::post('/paket', [PaketController::class, 'store'])->name('paket.store');
-    Route::put('/paket/{paket}', [PaketController::class, 'update'])->name('paket.update');
-    Route::delete('/paket/{paket}', [PaketController::class, 'update'])->name('paket.update');
+    Route::patch('/paket/{paket}', [PaketController::class, 'update'])->name('paket.update');
+    Route::delete('/pakets/{paket}', [PaketController::class, 'destroy']);
+
 });
 
 Route::middleware('web')->group(function () {
 
+
     Route::get('/reviews', [ReviewController::class, 'all'])->name('review.all');
+    Route::post('/review', [ReviewController::class, 'store'])->name('review.store');
     Route::get('/review/{review}', [ReviewController::class, 'show'])->name('review.show');
 
     Route::get('/partners', [PartnerController::class, 'all'])->name('partner.all');
@@ -58,7 +68,12 @@ Route::middleware('web')->group(function () {
     Route::get('/satuans', [SatuanController::class, 'all'])->name('satuan.all');
     Route::get('/satuan/{satuan}', [SatuanController::class, 'show'])->name('satuan.show');
 
+    Route::get('/paket/naskot/limited', [PaketController::class, 'getNasiKotakLimited']);
     Route::get('/pakets', [PaketController::class, 'all'])->name('paket.all');
-    Route::get('/paket/{paket}', [PaketController::class, 'show'])->name('paket.show');
+    // Route::get('/paket/{paket}', [PaketController::class, 'show'])->name('paket.show');
+    Route::get('/pakets/{paket}', [PaketController::class, 'getById'])->name('getById');
+
     Route::get('/paket/{paket}/items', [PaketController::class, 'items'])->name('paket.items');
+    Route::get('/paket/kategori/{kategori}', [PaketController::class, 'getByCategory']);
 });
+
