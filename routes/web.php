86241c5f2510d095b0429f12361651914cb85794
Vunix99 <rian.beda246@gmail.com>
2024-11-14 16:45:51 +0700

    feat: add new route to access web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaketController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -39,6 +41,14 @@ Route::get('/nasikotak', function () {
     return utama('nasikotak.index');
 });
 
+Route::get('/nasikotak/detail', function () {
+    return utama('nasikotak.show');
+});
+
+Route::get('/prasmanan/detail', function () {
+    return utama('prasmanan.show');
+});
+
 
 if (! function_exists('admin'))
 {
@@ -50,3 +60,44 @@ if (! function_exists('admin'))
 Route::get('/login', function () {
     return admin('login');
 });
+
+Route::get('/admin/prasmanan', function () {
+    return admin('prasmanan');
+});
+
+Route::get('/admin/edit/prasmanan', function () {
+    return admin('add_prasmanan');
+});
+
+Route::get('/admin/naskot', function () {
+    return admin('naskot');
+});
+
+Route::get('/admin/edit/naskot', function () {
+    return admin('add_naskot');
+});
+
+Route::get('/admin/ulasan', function () {
+    return admin('ulasan');
+});
+
+Route::get('/admin/account', function () {
+    return admin('account');
+});
+
+Route::get('/admin/isimenu', function () {
+    return admin('isi_menu');
+});
+
+Route::get('/admin/edit/isimenu', function () {
+    return admin('add_isimenu');
+});
+
+
+Route::post('/admin/paket', [PaketController::class, 'store'])->name('admin.paket.store');
+
+Route::get('/dashboard', function () {
+    return admin('dashboard'); // Ensure you have a corresponding view
+})->name('dashboard'); // Name the route
+
+
